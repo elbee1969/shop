e6e036f9d94b57a6318b3c6ef270dc2599e33cea
Laurent Berthelot <laurent.berthelot1969@gmail.com>
2020-02-17 15:48:08 +0100

    fonctions alea ajoutées

--- magasin-web/src/app/saisie/saisie.component.ts
+++ magasin-web/src/app/saisie/saisie.component.ts
@@ -11,6 +11,8 @@ import { HttpClient } from '@angular/common/http';
 export class SaisieComponent implements OnInit {
 
   public checkoutForm: FormGroup;
+  public capacity: Array<number> = [];
+  public articles: Array<number> = [];
 
     constructor(
       private formBuilder: FormBuilder, private http: HttpClient
@@ -19,6 +21,7 @@ export class SaisieComponent implements OnInit {
         caisses: '',
         clients: ''
       });
+
     }
   
     ngOnInit() {
@@ -28,8 +31,30 @@ export class SaisieComponent implements OnInit {
   
     onSubmit() {
      
+      let nbrCashRegisters = this.checkoutForm.value.caisses;
+      let nbrClients = this.checkoutForm.value.clients;
+      
+      
 
+     for(let i=0; i<nbrCashRegisters;i++) {
+        this.capacity.push(Math.floor(Math.random()*(40-20)+20));
+      }
+
+      for(let i=0; i<nbrClients;i++) {
+        this.articles.push(Math.floor(Math.random()* Math.floor(39) + 1));
+      }
+
+      console.log(nbrCashRegisters);
+      console.log(' tab capacity '+this.capacity);
+      console.log(' tab article '+this.articles);
+      console.log ('caisses : '+ nbrCashRegisters, 'Clients : '+ nbrClients);
         console.log(this.checkoutForm.value)
       this.http.post<object>('http://localhost:9000/shop', this.checkoutForm.value)
     }
+
+    alea(max: number) {
+      
+      return Math.floor(Math.random()* Math.floor(max) + 1)
+    };
+
   }
