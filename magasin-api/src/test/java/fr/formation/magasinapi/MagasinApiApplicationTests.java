82c9a04cf1d7617ca7008b49bd8f0543df988dc4
Tomas Lobgeois <lobgeois.tomas@free.fr>
2020-02-17 15:07:21 +0100

    add magasin-api

---
+++ magasin-api/src/test/java/fr/formation/magasinapi/MagasinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.magasinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagasinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
